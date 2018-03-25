0023c976d7cdb98c8428c2c658e65a90d1e6261f
Mahilet Hailemariam <mehalu15jan2013@gmail.com>
2018-03-25 00:19:42 -0700

    added glup path

--- nsc-practicum-winter2018-web/nsc-practicum-winter2018-web/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/message', function(req, res, next) {
-  res.json('Welcome To React from routes/index.js');
-});
-
-module.exports = router;
